a1888139636e76b8cf029bdf6cb32bdaafaa9106
SaurabhGoyal1981 <131611732+SaurabhGoyal1981@users.noreply.github.com>
2023-04-24 13:15:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import pandas
 import requests
+import snowflake.connector
 streamlit.title('My Parents New Healthy Diner')
 streamlit.title('BreakfastMenu')
 streamlit.text('😊Omega 3 a& Blueberry')
